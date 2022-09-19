854f18d337ad3960aa95e300a23c47cecfd15b8b
realtyz <realtyz@outlook.com>
2022-09-19 13:34:24 +0800

    [add] first commit

---
+++ lua/keybindings.lua
@@ -0,0 +1,45 @@
+local map = vim.api.nvim_set_keymap
+local opt = {noremap = true, silent = true }
+local pluginKeys = {}
+
+map("n", "<C-p>", ":Telescope find_files<CR>", opt)
+
+-- Alt + hjkl  窗口之间跳转
+map("n", "<A-h>", "<C-w>h", opt)
+map("n", "<A-j>", "<C-w>j", opt)
+map("n", "<A-k>", "<C-w>k", opt)
+map("n", "<A-l>", "<C-w>l", opt)
+
+-- bufferline
+-- 左右Tab切换
+map("n", "<C-h>", ":BufferLineCyclePrev<CR>", opt)
+map("n", "<C-l>", ":BufferLineCycleNext<CR>", opt)
+-- 关闭
+--"moll/vim-bbye"
+map("n", "<C-w>", ":Bdelete!<CR>", opt)
+map("n", "<leader>bl", ":BufferLineCloseRight<CR>", opt)
+map("n", "<leader>bh", ":BufferLineCloseLeft<CR>", opt)
+map("n", "<leader>bc", ":BufferLinePickClose<CR>", opt)
+
+map("n", "<A-m>", ":NvimTreeToggle<CR>", opt)
+-- 列表快捷键
+pluginKeys.nvimTreeList = {
+  -- 打开文件或文件夹
+  { key = {"<CR>", "o", "<2-LeftMouse>"}, action = "edit" },
+  -- 分屏打开文件
+  { key = "v", action = "vsplit" },
+  { key = "h", action = "split" },
+  -- 显示隐藏文件
+  { key = "i", action = "toggle_custom" }, -- 对应 filters 中的 custom (node_modules)
+  { key = ".", action = "toggle_dotfiles" }, -- Hide (dotfiles)
+  -- 文件操作
+  { key = "<F5>", action = "refresh" },
+  { key = "a", action = "create" },
+  { key = "d", action = "remove" },
+  { key = "r", action = "rename" },
+  { key = "x", action = "cut" },
+  { key = "c", action = "copy" },
+  { key = "p", action = "paste" },
+  { key = "s", action = "system_open" },
+}
+return pluginKeys
